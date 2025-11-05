ed221079faaa68b9d1a1bcf29c5ba24fc7c5cbd2
Talita Saraiva T. Nosse <68567312+tahsaraiva@users.noreply.github.com>
2025-11-05 19:18:40 -0400

    Update desafios.py
    add return com mensagem bem vindo

--- desafios.py
+++ desafios.py
@@ -18,7 +18,7 @@ def mostrar_mensagem_inicial():
     Exibe uma mensagem de boas-vindas ao desafio.
     Retorno esperado: string com a mensagem "Bem-vindo ao Desafio de Git!"
     """
-    pass
+    return "Bem-vindo ao Desafio de Git!"
 
 def listar_comandos_git_basicos():
     """
