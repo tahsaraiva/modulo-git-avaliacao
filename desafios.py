e33e38a6a21d69e14112a6d652da408df8823d58
Talita Saraiva <arena.talita@gmail.com>
2025-11-09 19:05:14 -0400

    IMplementa função mostrar_mensagem_inicial

--- desafios.py
+++ desafios.py
@@ -26,7 +26,7 @@ def listar_comandos_git_basicos():
     Exemplo de saída:
     ["git init", "git add", "git commit", "git status", "git push"]
     """
-    return ["git init", "git add", "git commit", "git status", "git push"]
+    pass
 
 
 def criar_mensagem_commit(funcao_nome):
@@ -36,7 +36,7 @@ def criar_mensagem_commit(funcao_nome):
     criar_mensagem_commit("listar_comandos_git_basicos") ->
     "Implementa função listar_comandos_git_basicos"
     """
-    return "Implementa função listar_comandos_git_basicos"
+    pass
 
 
 def verificar_tag_valida(tag):
@@ -44,7 +44,7 @@ def verificar_tag_valida(tag):
     Verifica se uma tag está no formato 'vX.Y' (ex: v1.0, v2.1).
     Retorna True se o formato for válido, caso contrário False.
     """
-    return "Retorna True se o formato for válido, caso contrário False."
+    pass
 
 
 def gerar_relatorio_final(funcoes_concluidas):
@@ -57,4 +57,4 @@ def gerar_relatorio_final(funcoes_concluidas):
     ->
     "Desafio concluído! 2 funções implementadas com sucesso."
     """
-    return "Relatorio final completo"
+    pass
